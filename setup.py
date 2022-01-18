a5d423914c2920c6a144744774b1d3856a467b73
Craig de Stigter <craig@destigter.nz>
2022-01-19 09:29:03 +1300

    Bump version to 0.2.0 and add tests for python 3.10

--- setup.py
+++ setup.py
@@ -8,31 +8,35 @@ def get_version():
     """
     Return package version as listed in `__version__` in `init.py`.
     """
-    init_py = open(os.path.join('mpathy', '__init__.py')).read()
+    init_py = open(os.path.join("mpathy", "__init__.py")).read()
     return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
 
 
 setup(
-    name='django-mpathy',
-    description='''Simple hierarchical data using materialised path, in a postgres database.''',
+    name="django-mpathy",
+    description="""Simple hierarchical data using materialised path, in a postgres database.""",
     version=get_version(),
-    author='Craig de Stigter',
-    author_email='craig.ds@gmail.com',
-    url='http://github.com/craigds/django-mpathy',
-    packages=['mpathy'],
-    install_requires=['psycopg2>=2.7', 'Django>=2.2',],
+    author="Craig de Stigter",
+    author_email="craig.ds@gmail.com",
+    url="http://github.com/craigds/django-mpathy",
+    packages=["mpathy"],
+    install_requires=[
+        "psycopg2>=2.7",
+        "Django>=2.2",
+    ],
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Topic :: Database',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Topic :: Utilities',
+        "Development Status :: 3 - Alpha",
+        "Topic :: Database",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Utilities",
     ],
 )
